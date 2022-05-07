84975d1f0c6b7fb1957fd84e003d5c4aae9602b7
epwalsh <epwalsh10@gmail.com>
2022-05-06 17:55:19 -0700

    tick version

--- beaker_run.py
+++ beaker_run.py
@@ -13,7 +13,7 @@ import yaml
 from beaker import Beaker, CurrentJobStatus, ExperimentSpec, TaskResources
 from rich import pretty, print, traceback
 
-VERSION = "1.1.3"
+VERSION = "1.1.4"
 
 
 class TermInterrupt(Exception):
